f62095f85104df995d0bd7631d06b4b6b6e03f79
MonikaJassova <monika.jassova@iohk.io>
2024-04-29 19:59:46 +0200

    mjs file

---
+++ src/testContainers.mjs
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S node --experimental-specifier-resolution=node
+import {
+    GenericContainer,
+    Wait
+  } from 'testcontainers';
+
+const environment = await new GenericContainer('ghcr.io/midnight-ntwrk/proof-server:2.0.6')
+.withEnvironment({ RUST_BACKTRACE: 'full' })
+.withCommand(['midnight-proof-server --network devnet'])
+.withExposedPorts(6300)
+.withHealthCheck({
+  test: ['CMD', 'curl', '-f', 'http://localhost:6300/version'],
+  interval: 30_000,
+  timeout: 5000,
+  retries: 3,
+  startPeriod: 10_000
+})
+.withWaitStrategy(Wait.forLogMessage('Actix runtime found; starting in Actix runtime'))
+.start();
+
+console.log('proof server started')
